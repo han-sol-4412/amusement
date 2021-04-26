29e7445be720ee86f38e011bc65a896a4f72d6cb
han-sol-4412 <gksthf2444@hanmail.net>
2021-04-26 11:00:31 +0900

    INPUT클래스 구현

--- Input.java
+++ Input.java
@@ -18,27 +18,41 @@ public class Input {
 	
 	public int ticketType() {
 		// 권종 입력받기
-		return 0;
+		System.out.println("권종을 선택하세요.\n"
+				+ "1. 주간권\n"
+				+ "2. 야간권");
+		return stdIn.nextInt();
 	}
 	
 	public String idNum() {
 		//주민번호 입력받기
-		return null;
+		System.out.println("주민번호를 입력하세요. (\"-\"를 제외하고 입력하시오.)");
+		return stdIn.next();
 	}
 	
 	public int ticketCount() {
 		//티켓 갯수 입력받기
-		return 0;
+		System.out.println("몇개를 주문하시겠습니까? (최대 10개)");
+		return stdIn.nextInt();
 	}
 	
 	public int discountCode() {
 		// 우대사항 입력받기
-		return 0;
+		System.out.println("우대사항을 선택하세요.\n"
+				+ "1. 없음 (나이 우대 자동처리)\n"
+				+ "2. 장애인 \n"
+				+ "3. 국가 유공자\n"
+				+ "4. 다자녀 \n"
+				+ "5. 임산부");
+		return stdIn.nextInt();
 	}
 	
 	public int continueBuyTicket() {
 		// 발권 진행 여부 입력받기
-		return 0;
+		System.out.println("계속 발권하시겠습니까?\n"
+				+ "1. 티켓 발권\n"
+				+ "2. 종료");
+		return stdIn.nextInt();
 	}
 	
 }
