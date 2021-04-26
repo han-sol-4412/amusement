1b424cb1a7fd0b48d2ae4f0eeeb7392c6eb0feb7
han-sol-4412 <gksthf2444@hanmail.net>
2021-04-26 15:05:23 +0900

    메인함수로 중간계산까지 검사

--- Main.java
+++ Main.java
@@ -3,8 +3,38 @@ package amusement;
 public class Main {
 	//메인클래스
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Input in = new Input();
+		ProcessingIdNum idnum = new ProcessingIdNum();
+		ProcessingAgeGroup ag = new ProcessingAgeGroup();
+		ProcessingOriginPrice op = new ProcessingOriginPrice();
+		ProcessingDiscount discount = new ProcessingDiscount();
+		CalculatorEachPrice price = new CalculatorEachPrice();
+		OutputPrint print = new OutputPrint();
+		SaveValue saveV;
+		
+		while(true) {
+			int ticketType = in.ticketType();
+			String idNum = in.idNum();
+			int ticketCount = in.ticketCount();
+			int discountCode = in.discountCode();
+			
+			int year = idnum.birth(idNum);
+			int age = idnum.age(year);
+			int agecode = ag.agePrice(age);
+			int originalPrice = op.originalPrice(ticketType, agecode);
+			double discountRate = discount.discount(discountCode);
+			int finalPrice = price.eachPrice(originalPrice, ticketCount, discountRate);
+			
+			//결과값 저장하기
+			saveV = new SaveValue(ticketType,agecode,ticketCount,originalPrice,discountCode,finalPrice);
+			print.printEachPrice(saveV);
+			
+			int continueBuyTicket;
+			continueBuyTicket = in.continueBuyTicket();
+			if(continueBuyTicket==2) break;
+		}
+		
+		
 	}
 
 }
