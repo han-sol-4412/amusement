d565d355e7df7474e753dd58c077dd3ab360ac95
han-sol-4412 <gksthf2444@hanmail.net>
2021-04-26 10:47:30 +0900

    상수 정하기

--- ConstValue.java
+++ ConstValue.java
@@ -1,5 +1,24 @@
 package amusement;
 	//상수 클래스
 public class ConstValue {
+	final static int DAY_CODE = 1;
+	final static int NIGHT_CODE = 2;
+	
+	// 권종과 나이에 따른 가격
+	final static int ADULT_DAY_PRICE = 56000;
+	final static int TEEN_DAY_PRICE = 47000;
+	final static int CHILD_DAY_PRICE = 44000;
+	final static int OLD_DAY_PRICE = 44000;
+	
+	final static int ADULT_NiGHT_PRICE = 46000;
+	final static int TEEN_NiGHT_PRICE = 40000;
+	final static int CHILD_NiGHT_PRICE = 37000;
+	final static int OLD_NiGHT_PRICE = 37000;
+	
+	//우대 할인율
+	final static double DISABLE_DISCOUNT_RATE = 0.4; 
+	final static double MERIT_DISCOUNT_RATE = 0.5;
+	final static double MULTICHILD_DISCOUNT_RATE = 0.2;
+	final static double PREGNANT_DISCOUNT_RATE = 0.15;
 
 }
