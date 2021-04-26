a6f766a8b235d40300f23baa938c8bda070aa75e
han-sol-4412 <gksthf2444@hanmail.net>
2021-04-26 14:34:15 +0900

    결과값 클래스로 저장하기

--- SaveValue.java
+++ SaveValue.java
@@ -1,5 +1,22 @@
 package amusement;
 	//주문 내역 저장 변수만 모아놓은 클래스
 public class SaveValue {
-
+	int ticketTypeCode; //티켓권종코드
+	int ageCode; //연령코드 
+	int ticketCount; // 티켓수량
+	int originalPrice; // 할인을 적용하기 전 가격
+	int discountCode; // 할인 적용 코드
+	int finalPrice; //할인을 적용한 후 가격
+	
+	//생성자로 값 저장하기
+	public SaveValue(int ticketTypeCode, int ageCode, int ticketCount, int originalPrice, int discountCode, int finalPrice) {
+		this.ticketTypeCode = ticketTypeCode;
+		this.ageCode = ageCode;
+		this.ticketCount = ticketCount;
+		this.originalPrice = originalPrice;
+		this.discountCode = discountCode;
+		this.finalPrice = finalPrice;
+	}
+	
+	
 }
